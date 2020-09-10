a5b2781e79102e75982aadadd7a3bbb64222c2e2
Colinr1994 <colinr1994@gmail.com>
2020-09-09 21:26:02 -0700

    first commit

---
+++ src/main/java/tracker/CovidTrackApplication.java
@@ -0,0 +1,13 @@
+package tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidTrackApplication.class, args);
+	}
+
+}
